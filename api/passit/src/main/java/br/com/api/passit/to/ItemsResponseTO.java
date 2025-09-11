45db1e7763a4d59635daf0c4a7de3572daf40810
rodrigoges <rgestevao@gmail.com>
2025-09-10 21:07:40 -0300

    fix: returning itemId in get items

--- api/passit/src/main/java/br/com/api/passit/to/ItemsResponseTO.java
+++ api/passit/src/main/java/br/com/api/passit/to/ItemsResponseTO.java
@@ -3,7 +3,10 @@ package br.com.api.passit.to;
 import br.com.api.passit.db.CategoryItemEnum;
 import br.com.api.passit.db.StatusItemEnum;
 
+import java.util.UUID;
+
 public record ItemsResponseTO(
+        UUID itemId,
         String title,
         String description,
         CategoryItemEnum category,
